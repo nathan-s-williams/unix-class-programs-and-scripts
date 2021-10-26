48d3497d43c6c93b083ab5cd2d46da9d0449d4ac
Nathan Williams <nate88williams@gmail.com>
2021-10-26 10:55:29 -0500

    Initial Commit

---
+++ ClassWork/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ClassWork
+//
+//  Created by Nathan Williams on 10/26/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
