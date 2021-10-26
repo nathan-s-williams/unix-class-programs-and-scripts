ca4be6acb05d44ade43770a5d475d73a7b9b9406
Nathan Williams <nate88williams@gmail.com>
2021-10-26 12:43:17 -0500

    Added while loop to main and renamed to sish.

---
+++ ClassWork/sish.c
@@ -0,0 +1,47 @@
+//
+//  main.c
+//  ClassWork
+//
+//  Created by Nathan Williams on 10/26/21.
+//
+
+#include <stdio.h>
+#include <string.h>
+
+extern int tokenize(char *cmd, char *commandArg[]);
+extern void excBuiltIn(int func);
+extern void excFile(char *commandArg[], int arrLength);
+
+int main(int argc, const char * argv[]) {
+    //Declare variables
+    char *commandArg[10];
+    while (1) {
+        
+        //Display the prompt
+        printf("sish> ");
+        
+        //Read command
+        char *cmd = NULL;
+        size_t sizeofString = 0;
+        getline(&cmd, &sizeofString ,stdin);
+        
+        //Tokenize input and return number of args passed.
+        int numArg = tokenize(cmd, commandArg);
+        
+        //Execute builtin
+        if(strcmp(commandArg[0], "exit") == 0) {
+            excBuiltIn(1);
+        }
+        else if (strcmp(commandArg[0], "cd")){
+            excBuiltIn(2);
+        }
+        else if (strcmp(commandArg[0], "history")){
+            excBuiltIn(3);
+        }
+        else{
+            excFile(commandArg, numArg);
+        }
+    }
+    
+    return 0;
+}
